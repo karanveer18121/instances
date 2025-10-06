45a88258c996635367c20bf0f2b916b70ca22842
karan <karanveermohan1010@gmail.com>
2025-10-06 10:32:40 +0000

    add keys

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ data "terraform_remote_state" "network_details" {
 resource "aws_instance" "my_vm" {
   ami           = "ami-02d26659fd82cf299"
   subnet_id     = data.terraform_remote_state.network_details.outputs.my_subnet
+  key_name      = data.terraform_remote_state.network_details.outputs.key_name
+  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
   instance_type = "t3.micro"
 
   tags = {
