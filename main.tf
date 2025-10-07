3b38aa9bd767df8eda72c7f8788a06fc60006d1b
karan <karanveermohan1010@gmail.com>
2025-10-07 12:54:55 +0000

    End Of Lab: Launching Multiple VMs

--- main.tf
+++ main.tf
@@ -16,6 +16,17 @@ source = "./modules/linux_node"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "student.18-vm1"
+  Name = "student.18-webserver-vm" 
   }
 }
+module "loadbalancer" {
+  source = "./modules/linux_node"
+  instance_count = 2
+  ami = "ami-02d26659fd82cf299"
+  instance_type = "t3.micro"
+  key_name = data.terraform_remote_state.network_details.outputs.key_name
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+  tags = { Name = "student.18-loadbalancer-vm" }
+}
+
